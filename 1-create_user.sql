e9a362e72d76af88da6f33a9395002d849c686ed
f-Ayuk <f.ayuk@alustudent.com>
2023-12-22 04:44:39 -0800

    Create the MySQL server user user_0d_1 with privileges and set password

---
+++ 1-create_user.sql
@@ -0,0 +1,14 @@
+-- Create the MySQL server user "user_0d_1" with privileges and set password
+
+-- Step 1: Create the user "user_0d_1" if it does not exist
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Step 2: Grant all privileges to the user "user_0d_1" on all databases
+
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- Step 3: Revoke specific privileges from the user "user_0d_1"
+-- (Optional: Only if certain privileges need to be revoked)
+
+REVOKE AUDIT_ABORT_EXEMPT, FIREWALL_EXEMPT, AUTHENTICATION_POLICY_ADMIN, GROUP_REPLICATION_STREAM, PASSWORDLESS_USER_ADMIN, SENSITIVE_VARIABLES_OBSERVER, TELEMETRY_LOG_ADMIN ON *.* FROM 'user_0d_1'@'localhost';
